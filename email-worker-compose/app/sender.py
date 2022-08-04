45e6ed9fb8e2912c836a58f80f6a9ea5aedc3fe3
Bruno Riwerson Silva <riwerson@gmail.com>
2022-08-03 23:14:45 -0300

    Review com repositório do curso

--- email-worker-compose/app/sender.py
+++ email-worker-compose/app/sender.py
@@ -4,22 +4,22 @@ import json
 import os
 from bottle import Bottle, request
 
+
 class Sender(Bottle):
     def __init__(self):
         super().__init__()
         self.route('/', method='POST', callback=self.send)
         
-        redis_host = os.getenv('REDIS_HOST', 'fake')
+        redis_host = os.getenv('REDIS_HOST', 'queue')
         self.fila = redis.StrictRedis(host=redis_host, port=6379, db=0)
-        
-        db_host = os.getenv('DB_HOST', 'fake')
-        db_user = os.getenv('DB_USER', 'fake')
-        db_name = os.getenv('DB_NAME', 'fake')
+
+        db_host = os.getenv('DB_HOST', 'db')
+        db_user = os.getenv('DB_USER', 'postgres')
+        db_name = os.getenv('DB_NAME', 'sender')
         dsn = f'dbname={db_name} user={db_user} host={db_host}'
         self.conn = psycopg2.connect(dsn)
-
-
-    def register_message(self, assunto, mensagem):        
+        
+    def register_message(self, assunto, mensagem):
         SQL = 'INSERT INTO emails (assunto, mensagem) VALUES (%s, %s)'
         cur = self.conn.cursor()
         cur.execute(SQL, (assunto, mensagem))
@@ -29,14 +29,14 @@ class Sender(Bottle):
         msg = {'assunto': assunto, 'mensagem': mensagem}
         self.fila.rpush('sender', json.dumps(msg))
 
-        print('Mensagem registrada!')
-    
+        print('Mensagem registrada !')
+
     def send(self):
         assunto = request.forms.get('assunto')
         mensagem = request.forms.get('mensagem')
 
         self.register_message(assunto, mensagem)
-        return 'Mensagem enfileirada! Assunto {} - Mensagem {}'.format(
+        return 'Mensagem enfileirada ! Assunto: {} Mensagem: {}'.format(
             assunto, mensagem
         )
 
--- email-worker-compose/worker/worker.py
+++ email-worker-compose/worker/worker.py
@@ -5,7 +5,7 @@ from time import sleep
 from random import randint
 
 if __name__ == '__main__':
-    redis_host = os.getenv('REDIS_HOST', 'fake')
+    redis_host = os.getenv('REDIS_HOST', 'queue')
     r = redis.Redis(host=redis_host, port=6379, db=0)
     print('Aguardando mensagens ...')
     while True:
